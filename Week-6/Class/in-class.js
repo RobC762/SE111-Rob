33c3ae8c290affdb1e0a0782421d0dca182088b6
Cady <rgcady@email.neit.edu>
2025-11-04 10:48:33 -0500

    classwork

---
+++ Week-6/Class/in-class.js
@@ -0,0 +1,6 @@
+//In class JavScript example
+
+var choice = prompt("What operation would you like to perform (+ or *)");
+var num1 = prompt("What is the first number?")
+var num2 = prompt("What is the second number?")
+console.log()
